da6c58b113d54e9ab26e96781e54938543b20bc2
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-03-14 15:18:31 +0100

    ex07 shorted

---
+++ ex07/filterwords_short.py
@@ -0,0 +1,5 @@
+import sys, string
+
+if not len(sys.argv) != 3 or sys.argv[1].isnumeric() or not sys.argv[2].isnumeric():
+	print([x for x in [''.join(c for c in w if c not in string.punctuation)
+		for w in sys.argv[1].split()] if len(x) > int(sys.argv[2])])
