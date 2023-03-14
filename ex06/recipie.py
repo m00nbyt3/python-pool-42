f9af65b8e0c49ac092779c07957c27f4b023089e
Yago Carro <ycarro@c1r1s1.42madrid.com>
2023-03-14 14:30:12 +0100

    ex06 done

--- ex06/recipie.py
+++ ex06/recipie.py
@@ -34,13 +34,79 @@ def show_recipies(cookbook):
 		print(' '+str(i)+'.', data)
 
 def get_recipie(name):
-	print("Detalles de:", name)
-	for i, data in enumerate(cookbook[name]):
-		if i == 0:
-			print(' '+data+':', end = ' ')
-			print(*cookbook[name][data], sep = ', ')
-		else:
-			print(' '+data+':', cookbook[name][data])
+	try:
+		print("Detalles de:", name)
+		for i, data in enumerate(cookbook[name]):
+			if i == 0:
+				print(' '+data+':', end = ' ')
+				print(*cookbook[name][data], sep = ', ')
+			else:
+				print(' '+data+':', cookbook[name][data])
+	except:
+		print("Error al obtener la receta")
 
 def remove_recipie(name):
-	del cookbook[name]
+	try:
+		del cookbook[name]
+	except:
+		print("Error al eliminar la receta")
+
+def add_recipie():
+	name = input("Enter a name: ")
+	ingredients = []
+	print("Enter ingredients:")
+	while True:
+			toadd = input()
+			if not toadd:
+				break
+			ingredients.append(toadd)
+	meal = input("Enter a meal type: ")
+	prep_time = input("Enter a preparation time: ")
+	cookbook[name] = dict(ingredients = ingredients, meal = meal, prep_time = prep_time)
+
+def menu():
+	print("Welcome to the Python Cookbook!")
+	print("List of available options:")
+	print("  1. Add a recipie")
+	print("  2. Delete a recipie")
+	print("  3. Print a recipie")
+	print("  4. Print the cookbook")
+	print("  5. Quit")
+	print("\nPlease select an option:")
+	return input(">> ")
+
+def selector(option):
+	if option == 1:
+		print('\n')
+		add_recipie()
+	elif option == 2:
+		print('\n')
+		name = input("Enter the name of the recipe to remove: ")
+		print('\n')
+		remove_recipie(name)
+	elif option == 3:
+		print('\n')
+		name = input("Enter the name of the recipie to get details: ")
+		print('\n')
+		get_recipie(name)
+	elif option == 4:
+		print('\n')
+		show_recipies(cookbook)
+	elif option == 5:
+		return True
+	else:
+		return False
+
+
+def main():
+	quit = 0
+	while not quit:
+		option = menu()
+		if not option.isnumeric():
+			print("\nOpcion invalida\n")
+			continue
+		quit = selector(int(option))
+		print('\n')
+
+if __name__ == '__main__':
+	main()
